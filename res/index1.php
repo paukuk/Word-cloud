32f581237a7b09f31410b22d3e726e85eb5a50a6
paukuk <paukuk@gmail.com>
2014-09-03 13:37:53 +0200

    Create index1.php

---
+++ res/index1.php
@@ -0,0 +1,13 @@
+<HTML>
+<body>
+    <!-- ... -->
+    <script src="https://www.java.com/js/deployJava.js"></script>
+    <script> 
+        var attributes = {
+            code:'components.WordCloudURL', width:300, height:300} ; 
+        var parameters = {jnlp_href: 'WordCloudURL.jnlp'} ; 
+        deployJava.runApplet(attributes, parameters, '1.7'); 
+    </script>
+    <!-- ... -->
+</body>
+</HTML>
