2028985de39c9d2d7b8cc9d9eb001e018de0d6de
Paulius Kukulskis <paukuk@gmail.com>
2014-09-03 16:59:13 +0200

    Create Index.php

---
+++ res/Index.php
@@ -0,0 +1,11 @@
+<?php>
+<HTML>
+<body>
+<applet width=600 height=270 code="WCURL" 
+  type="application/x-java-applet;jpi-version=7" 
+  archive="WCURL.jar"> 
+   no applet
+</applet>
+</body> 
+</HTML>
+<?>
